f806c88572a137ad24e45b03f73d6b2a3c148248
nakanishi <ponzxp@gmail.com>
2023-10-25 22:08:07 +0900

    add code

---
+++ pushbutton.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+
+# GPIO pin
+BUTTON_PIN = 18
+
+def button_callback(channel):
+	print("Nice Push!!")
+
+# BCM mode
+GPIO.setmode(GPIO.BCM)
+
+# pull up
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD.UP)
+
+# set event
+GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_callback, bouncetime=200)
+
+try:
+	input("Press Button!")
+finally:
+	GPIO.cleanup()
+	print("Bye!")
